60b51b35df04087570865b65194e5c98f3d27ca2
Joshva115 <Joshua.Chantler@hotmail.com>
2026-01-10 16:35:10 +1100

    Count Words: Completed

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
-#Need to define the path to the book to open it
+#This function will open the book and count how many words there found within the book.
 def get_book_text():
     with open("./books/frankenstein.txt", 'r', encoding='utf-8') as f:
         content = f.read()
-        print(content)
+        wordSplitted = content.split()
+        wordCount = 0
+        for word in wordSplitted:
+            wordCount +=1
         f.closed
+        print(f"Found {wordCount} total words")
 def main():
     get_book_text()
 
