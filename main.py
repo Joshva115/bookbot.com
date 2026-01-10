d7036199e186ccfe3094fcc0801dd624bfbf5992
Joshva115 <Joshua.Chantler@hotmail.com>
2026-01-11 08:47:58 +1100

    countCharacters: Completed, minor refactoring including as well

--- main.py
+++ main.py
@@ -1,9 +1,15 @@
-from stats import get_book_text 
+from stats import get_book_text, get_word_count, word_to_lower_case, adding_words_to_dict
 
 def main():
+    wordSplit = ""
     wordCount = 0
-    wordCount = get_book_text(wordCount)
-    print(wordCount)
+    wordDict = {}
+    wordSplit = get_book_text(wordSplit)
+    wordCount = get_word_count(wordSplit,wordCount)
+    print(f"Found {wordCount} total words")
+    wordSplit = word_to_lower_case(wordSplit)
+    wordDict = adding_words_to_dict(wordSplit,wordDict)
+    print(wordDict)
 
 
 
--- stats.py
+++ stats.py
@@ -1,8 +1,27 @@
-#This function will open a book and count the amount of words that appear in the book
-def get_book_text(wordCount):
+#This function will open a book and
+def get_book_text(wordSplit):
     with open("./books/frankenstein.txt", 'r', encoding='utf-8') as f:
         content = f.read()
-        wordSplitted = content.split()
-    for word in wordSplitted:
+        wordSplit = content.split()
+        return wordSplit
+
+#This function returns the wordCount in the entire book that was parsed in.
+def get_word_count(wordSplit,wordCount):
+    for word in wordSplit:
         wordCount +=1
     return wordCount
+
+#Function iterates through the entire list and returns each value lower cased
+def word_to_lower_case(wordSplit):
+    lowered_list = [word.lower() for word in wordSplit]
+    return lowered_list
+
+#Function iterates through list and will add to the dictionary the count of each character.
+def adding_words_to_dict(wordSplit,wordDict):
+    for word in wordSplit:
+        for j in word:
+            if j not in wordDict:
+                wordDict[j] = 1
+            else:
+                wordDict[j] +=1
+    return wordDict
