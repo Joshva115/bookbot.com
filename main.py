66f3e034d9faa9c6859dcc818e99c26643b6c83d
Joshva115 <Joshua.Chantler@hotmail.com>
2026-01-10 16:03:24 +1100

    Finished: Read File

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
-print("greetings boots")
+#Need to define the path to the book to open it
+def get_book_text():
+    with open("./books/frankenstein.txt", 'r', encoding='utf-8') as f:
+        content = f.read()
+        print(content)
+        f.closed
+def main():
+    get_book_text()
+
+main()
