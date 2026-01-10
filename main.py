f6984d6586e6d9a7275e4f99a4ebbafe7e335343
Joshva115 <Joshua.Chantler@hotmail.com>
2026-01-10 22:22:26 +1100

    Refactoring code to be in a different .py file for reuseability.

--- main.py
+++ main.py
@@ -3,5 +3,10 @@ from stats import get_book_text
 def main():
     wordCount = 0
     wordCount = get_book_text(wordCount)
-    print(f"Found {wordCount} total words")
+    print(wordCount)
+
+
+
+
+
 main()
